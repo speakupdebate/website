dce1a0951b0a39f153be51f7de9423b5019ecc0b
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-12-06 19:16:02 -0500

    add stuff to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,6 +25,36 @@ export default function About() {
 									hope to empower more students in our community through our
 									coming winter, spring, and summer programs.
 								</p>
+								<h1
+									className="h3 pt-10"
+									data-aos="zoom-y-out"
+									data-aos-delay={300}
+								>
+									Why Speak Up?
+								</h1>
+								<p
+									className="text-left pt-4"
+									data-aos="zoom-y-out"
+									data-aos-delay={450}
+								>
+									The ability to deliver messages with confidence is a vital
+									skill that can have an enormous impact on one’s success.
+									Public speaking is useful in every area of life from personal
+									to professional. Confidence along with strong public speaking
+									skills help build leaders that this world needs.
+								</p>
+								<p
+									className="text-left pt-4"
+									data-aos="zoom-y-out"
+									data-aos-delay={600}
+								>
+									Speak Up is a non-profit run by experienced debaters and
+									successful high school students that works to transform
+									students into leaders. Through our free programs, we focus on
+									helping students, regardless of experience, gain confidence in
+									their speaking, persuasive, and researching abilities. We hope
+									to empower students to find their voice.
+								</p>
 							</div>
 						</div>
 					</div>
